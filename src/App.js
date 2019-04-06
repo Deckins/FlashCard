c8c9958ef62a82cb2b20ffea40402a7948dff033
Ying Hao Guo <yinghao.guo34@bcmail.brooklyn.cuny.edu>
2019-04-06 18:59:38 -0400

    bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import './button.css';
+import Button from './Image/button';
 import FlashCard from './FlashCard/FlashCard';
 
 class App extends Component {
@@ -8,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <FlashCard/>
+        <Bitton/>
       </div>
     );
   }
---
+++ src/Image/button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './button.css';
+class Bitton extends Component {
+            <div>
+                <button class="LeftButton" onClick = {prev}>
+                </button>
+                <button class="RightButton" onClick = {prev}>
+                </button>
+
+            </div>
+        );
+    }
+}
+
+export default Bitton;
