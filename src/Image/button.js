149a652d1f3e1646f0bd7c38985a4591e3fa738a
Ying Hao Guo <yinghao.guo34@bcmail.brooklyn.cuny.edu>
2019-04-06 19:01:02 -0400

    bugs

--- src/Image/button.js
+++ src/Image/button.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './button.css';
 class Bitton extends Component {
-            <div>
-                <button class="LeftButton" onClick = {prev}>
-                </button>
-                <button class="RightButton" onClick = {prev}>
-                </button>
-
-            </div>
+    render() {
+        return (
+            <button class="LeftButton" onClick = {prev}>
+            </button>
+            <button class="RightButton" onClick = {prev}>
+            </button>
         );
     }
 }
